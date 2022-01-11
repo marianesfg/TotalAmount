b76e666d4cb78fa2f4ca4ee982c94f63af07848a
Mariane <mariane.sfgodoy@gmail.com>
2022-01-10 23:59:06 -0300

    First commit.

---
+++ Model/Purchase.cs
@@ -0,0 +1,11 @@
+﻿namespace TotalAmount.Model
+{    
+    public class Purchase
+    {
+        public int Cod { get; set; }
+
+        public int Qtd { get; set; }
+
+        public decimal Amount { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using TotalAmount.Model;
+
+namespace TotalAmount
+{
+    class Program
+    {
+        static void Main()
+        {
+            char sair;
+
+            decimal dAmount;
+
+            int iCod, iQtd = 0;
+
+            List<Purchase> compras = new List<Purchase>();
+
+            NumberStyles style = NumberStyles.Number | NumberStyles.AllowCurrencySymbol;
+            CultureInfo culture = CultureInfo.CreateSpecificCulture("en-GB");
+
+            string sAmount, sCod, sQtd = "";
+
+            do
+            {
+                sair = 'n';
+                Purchase compra = new Purchase();
+
+                Console.Write("Código do Produto: ");                
+                sCod = Console.ReadLine();
+                if (int.TryParse(sCod, out iCod))
+                    compra.Cod = iCod;
+                else
+                {
+                    Console.WriteLine("Formato de Código inválido!");
+                    sair = 's';
+                }                
+
+                if (sair == 'n')
+                {
+                    Console.WriteLine();
+                    Console.Write("Quantidade do Produto: ");
+                    sQtd = Console.ReadLine();
+                    if (int.TryParse(sQtd, out iQtd))
+                        compra.Qtd = iQtd;
+                    else
+                    {
+                        Console.WriteLine("Quantidade de Produto inválida!");
+                        sair = 's';
+                    }
+                }
+
+                if (sair == 'n')
+                {
+                    Console.WriteLine();
+                    Console.Write("Valor do Produto: ");
+                    sAmount = Console.ReadLine();
+                    if (decimal.TryParse(sAmount, style, culture, out dAmount))
+                        compra.Amount = dAmount;
+                    else
+                    {
+                        Console.WriteLine("Valor do Produto inválido!");
+                        sair = 's';
+                    }
+                }                
+
+                Console.WriteLine();              
+
+                if (sair == 'n')
+                {
+                    compras.Add(compra);
+
+                    Console.WriteLine();
+
+                    Console.Write("Finalizar? S (sim) / N (não): ");
+                    sair = char.Parse(Console.ReadLine());
+                    Console.WriteLine();
+                }
+
+                Console.WriteLine("-----------------------------------------------------------");
+
+            } while (sair != 's' && sair != 'S');
+
+            decimal total = 0;
+
+            foreach (Purchase c in compras)            
+                total += c.Amount * c.Qtd;
+
+            Console.WriteLine($"VALOR A PAGAR: {total.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
